d328085301a6479727d20203e0eba4d7149ca017
Emi-Ureta <emilia.ureta.toro@gmail.com>
2023-07-25 12:40:04 -0400

    Bet and number of lines, verification of amount to bet

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import random
+
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -12,4 +22,53 @@ def deposit():
     return amount
 
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input(
+            f"Enter the number of lines you want to bet on (1-{MAX_LINES}) ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines")
+        else:
+            print("Please enter a number")
+    return lines
+
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} and ${MAX_BET}")
+        else:
+            print("Please enter a number")
+    return amount
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if total_bet > balance:
+            print(
+                f"You don't have enough balance to bet this amount. \nYour current balance is ${balance}")
+        else:
+            break
+    if 1 < lines:
+        print(
+            f"You are bettin ${bet} on {lines} lines. The total bet is ${total_bet}")
+    # print(balance, lines, bet)
+    else:
+        print(
+            f"You are bettin ${bet} on {lines} line. The total bet is ${total_bet}")
+
+
+main()
